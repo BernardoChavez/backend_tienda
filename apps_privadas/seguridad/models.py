5d4906d49ce78b2ca1c06ed37c722ede446476fd
armandopaco051-beep <armandopaco051@gmail.com>
2026-05-10 23:39:01 -0400

    Add BitacoraAuditoria model for audit logging
    bitacora

--- apps_privadas/seguridad/models.py
+++ apps_privadas/seguridad/models.py
@@ -63,3 +63,23 @@ class CodigoRecuperacion(models.Model):
 
     def __str__(self):
         return f"{self.usuario.username} - {self.codigo}"
+
+class BitacoraAuditoria(models.Model):
+    id_bitacora = models.BigAutoField(primary_key=True, db_column='id_bitacora')
+    fecha = models.DateField()
+    hora = models.TimeField()
+    entidad = models.CharField(max_length=100)
+    detalles = models.CharField(max_length=500)
+    accion = models.CharField(max_length=100)
+    usuarios_id = models.BigIntegerField(db_column='usuarios_id')
+
+    class Meta:
+        managed = False
+        db_table = '"auditoria"."bitacora"'
+        ordering = ['-fecha', '-hora', '-id_bitacora']
+        verbose_name = 'Bitacora de auditoria'
+        verbose_name_plural = 'Bitacoras de auditoria'
+
+    def __str__(self):
+        return f"{self.fecha} {self.hora} - {self.accion} - {self.entidad}"
+
