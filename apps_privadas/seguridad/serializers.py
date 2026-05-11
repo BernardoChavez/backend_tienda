c94b1c9285e015d3f9b8fc576d4c4ee66460bc17
Gerson <villarroelpoma2@gmail.com>
2026-05-11 15:19:17 -0400

    importacion de bitacora

--- apps_privadas/seguridad/serializers.py
+++ apps_privadas/seguridad/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps_privadas.seguridad.models import Usuario
+from apps_privadas.seguridad.models import Usuario, BitacoraAuditoria
 from django.contrib.auth.models import Group, Permission
 
 
