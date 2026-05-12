9230128ebfb56825bdbc336420b6b3dc89ba3331
Gerson <villarroelpoma2@gmail.com>
2026-05-11 23:25:28 -0400

    cambios middleware

--- apps_publicas/empresas/middleware.py
+++ apps_publicas/empresas/middleware.py
@@ -5,13 +5,15 @@ from django_tenants.utils import get_tenant_model
 class TenantMiddleware(TenantMainMiddleware):
     def get_tenant(self, domain_model, hostname):
         # Primero intenta por header X-Tenant (móvil/Flutter)
-        tenant_header = self.request.META.get('HTTP_X_TENANT')
-        if tenant_header:
-            TenantModel = get_tenant_model()
-            try:
-                return TenantModel.objects.get(schema_name=tenant_header)
-            except TenantModel.DoesNotExist:
-                pass
+        request = self.request if hasattr(self, 'request') else None
+        if request:
+            tenant_header = request.META.get('HTTP_X_TENANT')
+            if tenant_header:
+                TenantModel = get_tenant_model()
+                try:
+                    return TenantModel.objects.get(schema_name=tenant_header)
+                except TenantModel.DoesNotExist:
+                    pass
 
         # Si no hay header, usa subdominio (web)
         return super().get_tenant(domain_model, hostname)
